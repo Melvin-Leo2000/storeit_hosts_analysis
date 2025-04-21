fcd017f4a8b4408a09b45470e0ef606f82bc3328
MelvinLeo <melvin@autocomplete.io>
2025-04-21 23:02:12 +0800

    new map

--- Host_Dashboard.py
+++ Host_Dashboard.py
@@ -3,6 +3,11 @@ import pandas as pd
 import folium
 from streamlit_folium import st_folium
 from datetime import datetime
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
 
 
 
@@ -21,6 +26,16 @@ customer_gid = st.secrets["CUSTOMER_GID"]
 # OneMap API
 ONEMAP_EMAIL = st.secrets["ONEMAP_EMAIL"]
 ONEMAP_PASSWORD = st.secrets["ONEMAP_PASSWORD"]
+
+
+# # Google Sheet IDs
+# host_sheet_id = os.getenv("HOST_SHEET_ID")
+# host_gid = os.getenv("HOST_GID")
+# customer_gid = os.getenv("CUSTOMER_GID")
+
+# # OneMap API
+# ONEMAP_EMAIL = os.getenv("ONEMAP_EMAIL")
+# ONEMAP_PASSWORD = os.getenv("ONEMAP_PASSWORD")
 
 host_url = f"https://docs.google.com/spreadsheets/d/{host_sheet_id}/export?format=csv&gid={host_gid}"
 customer_url = f"https://docs.google.com/spreadsheets/d/{host_sheet_id}/export?format=csv&gid={customer_gid}"
@@ -51,8 +66,9 @@ def load_upaid_customer_data(url):
     df = pd.read_csv(url)
     df['Check-In Date'] = pd.to_datetime(df['Check-In Date'], errors='coerce')
     df['End Date'] = pd.to_datetime(df['End Date'], errors='coerce')
+
     df = df[
-        (df['Stage'] <= 1) &
+        (df['Stage'].isin([0, 1])) &
         (df['End Date'] > pd.Timestamp.today()) &
         (df['Matched Host'].notna()) &
         (df['Matched Host'] != "")
@@ -73,7 +89,7 @@ host_coords = (
 )
 
 # --- Map Visualization ---
-st.subheader("🗺️ Map of Exiting Hosts and Customers")
+st.subheader("🗺️ Map of Transacted Hosts and Customers")
 
 map_center = [1.3521, 103.8198]
 m = folium.Map(location=map_center, zoom_start=11)
@@ -109,29 +125,48 @@ for _, row in customers.iterrows():
             tooltip=f"Customer: {row['Username']} ↔ Host: {matched}"  
         ).add_to(m)
 
-# for _, row in unpaid_customers.iterrows():
-#     cust_loc = (row['Latitude'], row['Longitude'])
-#     folium.Marker(
-#         location=[row['Latitude'], row['Longitude']],
-#         tooltip=row['Username'],
-#         popup=f"Unpaid Customer: {row['Check-In Date'].date()} to {row['End Date'].date()}",
-#         icon=folium.Icon(color='white', icon='user')
-#     ).add_to(m)
-
-#     matched = row.get('Matched Host')
-#     if pd.notna(matched) and matched in host_coords:
-#         host_loc = (host_coords[matched]['Latitude'], host_coords[matched]['Longitude'])
-        
-#         folium.PolyLine(
-#             locations=[cust_loc, host_loc],
-#             color='blue',         # make it stand out
-#             weight=5,             # thicker line
-#             opacity=1,
-#             tooltip=f"Customer: {row['Username']} ↔ Host: {matched}"  
-#         ).add_to(m)
 
 
 st_folium(m, width=900, height=550)
+
+
+
+st.subheader("🗺️ Map of Ongoing Matched Customers and Hosts")
+um = folium.Map(location=map_center, zoom_start=11)
+
+# Plot hosts (blue)
+for _, row in hosts.iterrows():
+    folium.Marker(
+        location=[row['Latitude'], row['Longitude']],
+        tooltip=row['Username'],
+        popup=f"Host Available: {row['Available Start Date'].date()} to {row['Available End Date'].date()}",
+        icon=folium.Icon(color='blue', icon='home')
+    ).add_to(um)
+
+for _, row in unpaid_customers.iterrows():
+    cust_loc = (row['Latitude'], row['Longitude'])
+    folium.Marker(
+        location=[row['Latitude'], row['Longitude']],
+        tooltip=row['Username'],
+        popup=f"Unpaid Customer: {row['Check-In Date'].date()} to {row['End Date'].date()}",
+        icon=folium.Icon(color='white', icon='user')
+    ).add_to(um)
+
+    matched = row.get('Matched Host')
+    if pd.notna(matched) and matched in host_coords:
+        host_loc = (host_coords[matched]['Latitude'], host_coords[matched]['Longitude'])
+        
+        folium.PolyLine(
+            locations=[cust_loc, host_loc],
+            color='blue',         # make it stand out
+            weight=5,             # thicker line
+            opacity=1,
+            tooltip=f"Customer: {row['Username']} ↔ Host: {matched}"  
+        ).add_to(um)
+
+
+st_folium(um, width=900, height=550)
+
 
 
 st.subheader("📋 Customer Checkin Dates")
